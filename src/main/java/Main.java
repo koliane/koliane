47e5bf3bfc0aa437cb4ee5814cbb7c0dc6df19f0
Nick Tyshkovets <koliane555@mail.ru>
2021-10-09 00:05:51 +0300

    Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,6 @@ import core.application.commands.specification_command.SpecificationCommand;
 import core.application.executors.BaseExecutor;
 import core.application.executors.help.HelpExecutor;
 import core.application.executors.specification.SpecificationExecutor;
-import core.infrastructure.helpers.YamlHelper;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Map;
 
 //long m = System.currentTimeMillis();
 //System.out.println( (System.currentTimeMillis() - m));
@@ -18,29 +13,19 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-//        Path pathToYaml = Paths.get("D:\\projects\\flutter\\inject_test\\pubspec.yaml");
-//        Map map = YamlHelper.getYamlMap(pathToYaml);
+//        String[] newArgs = {
+////                "pd=D:\\projects\\flutter\\inject_test",
+//                "pd=D:\\projects\\flutter\\temp",
+//                "sd=D:\\projects\\flutter\\template\\spec.yaml",
+////                "sd=D:\\projects\\java\\koliane\\src\\main\\resources\\default\\spec.yaml",
+////                "add_entity_method",
+//                "init",
+////                "entity_name=AuthHello"
 //
-//        if(true) {
-//            throw new Exception("");
-//        }
-//        int size = args.length;
-//        for(int i=0; i < size;i++) {
-//            System.out.println(args[i]);
-//        }
-//
-        String[] newArgs = {
-                "pd=D:\\projects\\flutter\\inject_test",
-                "sd=D:\\projects\\flutter\\template\\spec.yaml",
-//                "sd=D:\\projects\\java\\koliane\\src\\main\\resources\\default\\spec.yaml",
-//                "add_entity_method",
-                "test",
-                "entity_name=AuthHello"
+//        };
+//        CommandRequest commandRequest = new CommandRequest(newArgs);
 
-        };
-        CommandRequest commandRequest = new CommandRequest(newArgs);
-
-//            CommandRequest commandRequest = new CommandRequest(args);
+            CommandRequest commandRequest = new CommandRequest(args);
             Command inputCommand = commandRequest.getCommand();
             BaseExecutor executor;
 
